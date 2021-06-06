406e151bb0f74ec3b3d78bb6a1c77a011de081cd
Leandra Fichtel <fichtel@is68.ifis.cs.tu-bs.de>
2021-06-06 22:10:24 +0200

    implemented transfer learning

---
+++ LAMA/scripts/run_experiments.py
@@ -0,0 +1,169 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+import argparse
+from LAMA.scripts.batch_eval_KB_completion import main as run_evaluation
+from LAMA.scripts.batch_eval_KB_completion import load_file
+from LAMA.lama.modules import build_model_by_name
+import pprint
+import statistics
+from os import listdir
+import os
+from os.path import isfile, join
+from shutil import copyfile
+from collections import defaultdict
+
+
+def run_experiments(
+    relations,
+    data_path_pre,
+    data_path_post,
+    input_param={
+        "lm": "bert",
+        "label": "bert_large",
+        "models_names": ["bert"],
+        "bert_model_name": "bert-large-cased",
+        "bert_model_dir": "LAMA/pre-trained_language_models/bert/cased_L-24_H-1024_A-16",
+    },
+    use_negated_probes=False,
+):
+    model = None
+    pp = pprint.PrettyPrinter(width=41, compact=True)
+
+    all_Precision1 = []
+    type_Precision1 = defaultdict(list)
+    type_count = defaultdict(list)
+
+    last_results_file = open("LAMA/last_results.csv", "w+")
+    results_file = open("/home/fichtel/BERTriple/results/{}.csv".format(input_param["label"]), "w+")
+
+    for relation in relations:
+        pp.pprint(relation)
+        PARAMETERS = {
+            "dataset_filename": "{}{}{}".format(
+                data_path_pre, relation["relation"], data_path_post
+            ),
+            "common_vocab_filename": "LAMA/pre-trained_language_models/common_vocab_cased.txt",
+            "template": "",
+            "bert_vocab_name": "vocab.txt",
+            "batch_size": 32,
+            "logdir": "/data/fichtel/BERTripel/LAMA_output/output",
+            "full_logdir": "/data/fichtel/BERTripel/LAMA_output/output/results/{}/{}".format(
+                input_param["label"], relation["relation"]
+            ),
+            "lowercase": False,
+            "max_sentence_length": 100,
+            "threads": -1,
+            "interactive": False,
+            "use_negated_probes": use_negated_probes,
+        }
+
+        if "template" in relation:
+            PARAMETERS["template"] = relation["template"]
+            if use_negated_probes:
+                PARAMETERS["template_negated"] = relation["template_negated"]
+
+        PARAMETERS.update(input_param)
+        #print(PARAMETERS)
+
+        args = argparse.Namespace(**PARAMETERS)
+
+        # see if file exists
+        try:
+            data = load_file(args.dataset_filename)
+        except Exception as e:
+            print("Relation {} excluded.".format(relation["relation"]))
+            print("Exception: {}".format(e))
+            continue
+
+        if model is None:
+            [model_type_name] = args.models_names
+            model = build_model_by_name(model_type_name, args)
+
+        Precision1 = run_evaluation(args, shuffle_data=False, model=model)
+        print("P@1 : {}".format(Precision1), flush=True)
+        all_Precision1.append(Precision1)
+        
+        last_results_file.write(
+            "{},{}\n".format(relation["relation"], round(Precision1 * 100, 2))
+        )
+        last_results_file.flush()
+
+        results_file.write(
+            "{},{}\n".format(relation["relation"], round(Precision1 * 100, 2))
+        )
+        results_file.flush()
+
+        if "type" in relation:
+            type_Precision1[relation["type"]].append(Precision1)
+            data = load_file(PARAMETERS["dataset_filename"])
+            type_count[relation["type"]].append(len(data))
+
+    mean_p1 = statistics.mean(all_Precision1)
+    print("@@@ {} - mean P@1: {}".format(input_param["label"], mean_p1))
+    last_results_file.close()
+    results_file.write(
+        "avg,{}\n".format(mean_p1)
+    )
+    results_file.close()
+
+    for t, l in type_Precision1.items():
+
+        print(
+            "@@@ ",
+            input_param["label"],
+            t,
+            statistics.mean(l),
+            sum(type_count[t]),
+            len(type_count[t]),
+            flush=True,
+        )
+
+    return mean_p1, all_Precision1
+
+
+def get_TREx_parameters(data_path_pre="LAMA/data/"):
+    relations = load_file("{}relations.jsonl".format(data_path_pre))
+    data_path_pre += "TREx/"
+    data_path_post = ".jsonl"
+    return relations, data_path_pre, data_path_post
+
+
+def run_all_LMs(parameters, LMs):
+    for ip in LMs:
+        print(ip["label"])
+        run_experiments(*parameters, input_param=ip, use_negated_probes=False)
+
+def start_custom_model_eval(model_dir):
+    print("2. T-REx")
+    parameters = get_TREx_parameters()
+    LMs = [
+        {
+            "lm": "bert",
+            "label": model_dir,
+            "models_names": ["bert"],
+            "bert_model_name": "bert-base-cased-finetuned",
+            "bert_model_dir": "/data/fichtel/BERTriple/models/{}".format(model_dir),
+        },
+    ]
+    run_all_LMs(parameters, LMs)
+
+
+if __name__ == "__main__":
+    print("2. T-REx")
+    parameters = get_TREx_parameters()
+    LMs = [
+        {
+            "lm": "bert",
+            "label": "bert_base",
+            "models_names": ["bert"],
+            "bert_model_name": "bert-base-cased",
+            "bert_model_dir": "LAMA/pre-trained_language_models/bert/cased_L-12_H-768_A-12",
+        },
+    ]
+    run_all_LMs(parameters, LMs)
+
+
--- add_pretraining_bert_trainer.py
+++ add_pretraining_bert_trainer.py
@@ -1,24 +1,42 @@
+import torch
 from transformers import BertForMaskedLM, BertModel, Trainer, TrainingArguments
 import json
 from transformers import BertTokenizer
-import torch
-#from torch.utils.data import DataLoader
-from transformers import AdamW
-import dill
 import os
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import shutil
 import argparse
-#from torchsummary import summary
 from shutil import copyfile
 import random
-
-
+import pandas as pd
+from itertools import chain, combinations
+from LAMA.scripts.run_experiments import start_custom_model_eval
 
 #functions
-def read_dataset(dataset_path, dictio_prop_template, query_type, sample, template):
+def get_queries_answers(dictio_prop_triple, query_type, omitted_props):
     queries = []
     answers = []
+    if dictio_prop_triple[query_type] == {}:
+        exit("dataset cannot be used for {}".format(query_type))
+    for prop in dictio_prop_triple[query_type]:
+        print(prop, len(dictio_prop_triple[query_type][prop]))
+        for datapoint in dictio_prop_triple[query_type][prop]:
+            if omitted_props and prop in omitted_props:
+                print("no training data for", prop)
+                break
+            else:
+                subj_label = datapoint["subj"]
+                prop =  datapoint["prop"]
+                obj_label = datapoint["obj"]
+                query_template = dictio_prop_template[prop][template]
+                if query_type == "subj_queries":
+                    queries.append(query_template.replace("[S]", "[MASK]").replace("[O]", obj_label))
+                    answers.append(query_template.replace("[S]", subj_label).replace("[O]", obj_label))
+                elif query_type == "obj_queries":
+                    queries.append(query_template.replace("[S]", subj_label).replace("[O]", "[MASK]"))
+                    answers.append(query_template.replace("[S]", subj_label).replace("[O]", obj_label))
+    return queries, answers
+
+def prepare_dataset(dataset_path, dictio_prop_template, query_type, sample, template, omitted_props):
     if os.path.exists(dataset_path):
         print("read given dataset", dataset_path)
         dictio_prop_triple = json.load(open(dataset_path, "r"))
@@ -36,49 +54,25 @@ def read_dataset(dataset_path, dictio_prop_template, query_type, sample, templat
         del dataset_file
         dataset_file_sample = open(dataset_path, "w")
         json.dump(dictio_prop_triple, dataset_file_sample)
+
+    all_queries = []
+    all_answers = []
     if query_type == "subjobj":
-        if dictio_prop_triple["subj_queries"] == {} or dictio_prop_triple["obj_queries"] == {}:
-            exit("dataset cannot be used for subjobj")
-        for prop in dictio_prop_triple["subj_queries"]:
-            for datapoint in dictio_prop_triple["subj_queries"][prop]:
-                subj_label = datapoint["subj"]
-                prop =  datapoint["prop"]
-                obj_label = datapoint["obj"]
-                query_template = dictio_prop_template[prop][template]
-                queries.append(query_template.replace("[S]", "[MASK]").replace("[O]", obj_label))
-                answers.append(query_template.replace("[S]", subj_label).replace("[O]", obj_label))
-        for prop in dictio_prop_triple["obj_queries"]:
-            for datapoint in dictio_prop_triple["obj_queries"][prop]:
-                subj_label = datapoint["subj"]
-                prop =  datapoint["prop"]
-                obj_label = datapoint["obj"]
-                query_template = dictio_prop_template[prop][template]
-                queries.append(query_template.replace("[S]", subj_label).replace("[O]", "[MASK]"))
-                answers.append(query_template.replace("[S]", subj_label).replace("[O]", obj_label))
+        queries, answers = get_queries_answers(dictio_prop_triple, "subj_queries", omitted_props)
+        all_queries = all_queries + queries
+        all_answers = all_answers + answers
+        queries, answers = get_queries_answers(dictio_prop_triple, "obj_queries", omitted_props)
+        all_queries = all_queries + queries
+        all_answers = all_answers + answers
     elif query_type == "subj":
-        if dictio_prop_triple["subj_queries"] == {}:
-            exit("dataset cannot be used for subj")
-        for prop in dictio_prop_triple["subj_queries"]:
-            for datapoint in dictio_prop_triple["subj_queries"][prop]:
-                subj_label = datapoint["subj"]
-                prop =  datapoint["prop"]
-                obj_label = datapoint["obj"]
-                query_template = dictio_prop_template[prop][template]
-                queries.append(query_template.replace("[S]", "[MASK]").replace("[O]", obj_label))
-                answers.append(query_template.replace("[S]", subj_label).replace("[O]", obj_label))
+        queries, answers = get_queries_answers(dictio_prop_triple, "subj_queries", omitted_props)
+        all_queries = all_queries + queries
+        all_answers = all_answers + answers
     elif query_type == "obj":
-        if dictio_prop_triple["obj_queries"] == {}:
-            exit("dataset cannot be used for obj")
-        for prop in dictio_prop_triple["obj_queries"]:
-            print(prop, len(dictio_prop_triple["obj_queries"][prop]))
-            for datapoint in dictio_prop_triple["obj_queries"][prop]:
-                subj_label = datapoint["subj"]
-                prop =  datapoint["prop"]
-                obj_label = datapoint["obj"]
-                query_template = dictio_prop_template[prop][template]
-                queries.append(query_template.replace("[S]", subj_label).replace("[O]", "[MASK]"))
-                answers.append(query_template.replace("[S]", subj_label).replace("[O]", obj_label))
-    return queries, answers
+        queries, answers = get_queries_answers(dictio_prop_triple, "obj_queries", omitted_props)
+        all_queries = all_queries + queries
+        all_answers = all_answers + answers
+    return all_queries, all_answers
 
 class MaskedDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
@@ -91,33 +85,9 @@ class MaskedDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
-if __name__ == "__main__":
-    os.environ['CUDA_VISIBLE_DEVICES'] = "1"
-    dictio_prop_template = json.load(open("/data/fichtel/BERTriple/templates.json", "r"))
-
-    #parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-train_file', help="training dataset name (LPAQAfiltered41/LPAQAfiltered25 or wikidata41/wikidata25)")
-    parser.add_argument('-sample', help="set how many triple should be used of each property at maximum (e.g. 500 (=500 triples per prop for each query type) or all (= all given triples per prop for each query type))")
-    parser.add_argument('-epoch', help="set how many epoches should be executed")
-    parser.add_argument('-template', help="set which template should be used (LAMA or label)")
-    parser.add_argument('-query_type', help="set which queries should be used during training (subjobj= subject and object queries, subj= only subject queries, obj= only object queries)")
-    
-    args = parser.parse_args()
-    print(args)
-    train_file = args.train_file
-    epoch = int(args.epoch)
-    template = args.template
-    sample = args.sample
-    query_type = args.query_type
-    assert(query_type in ["subjobj", "subj", "obj"])
-
-    #used LM
-    lm_name = 'bert-base-cased'
-    #pepare training dataset
-    #read datasets from path
-    train_queries, train_answers = read_dataset("/data/fichtel/BERTriple/training_datasets/{}_{}.json".format(train_file, sample), dictio_prop_template, query_type, sample, template)
-    print("check datapoints", train_queries[0], train_answers[0])
+def train(lm_name, train_file, sample, epoch, template, query_type, omitted_props):
+    train_queries, train_answers = prepare_dataset("/data/fichtel/BERTriple/training_datasets/{}_{}.json".format(train_file, sample), dictio_prop_template, query_type, sample, template, omitted_props)
+    print("check datapoints:", train_queries[0], train_answers[0])
     #use tokenizer to get encodings
     tokenizer = BertTokenizer.from_pretrained(lm_name)
     train_question_encodings = tokenizer(train_queries, truncation=True, padding='max_length', max_length=256)
@@ -128,8 +98,18 @@ if __name__ == "__main__":
     model = BertModel.from_pretrained(lm_name)
 
     print("start training")
-    lm_name_path = lm_name.replace("-", "_")
-    model_path = "/data/fichtel/BERTriple/models/{}_finetuned_{}_{}_{}_{}_{}".format(lm_name_path, train_file, sample, query_type, epoch, template)
+    lm_name_short = lm_name.split("-")
+    lm_name_capitals = lm_name_short[0].upper()[0] + lm_name_short[1].upper()[0] + lm_name_short[2].upper()[0]
+    if omitted_props:
+        props_string = "_"
+        omitted_props = sorted(omitted_props, key=lambda x: int("".join([i for i in x if i.isdigit()])))
+        for prop in omitted_props: 
+            props_string = props_string + prop
+    else:
+        props_string = ""
+    
+    model_path = "/data/fichtel/BERTriple/models/{}F_{}_{}_{}_{}_{}{}".format(lm_name_capitals, train_file, sample, query_type, epoch, template, props_string)
+    
     if os.path.exists(model_path):
         print("remove dir of model")
         shutil.rmtree(model_path)
@@ -159,3 +139,122 @@ if __name__ == "__main__":
     trainer.save_model(model_path)
     copyfile("/home/fichtel/BERTriple/LAMA/pre-trained_language_models/bert/cased_L-12_H-768_A-12/vocab.txt", model_path + "/vocab.txt")
     os.rename(model_path + "/config.json", model_path + "/bert_config.json")
+    return model_path.split("/")[-1]
+
+if __name__ == "__main__":
+    os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+    dictio_prop_template = json.load(open("/data/fichtel/BERTriple/templates.json", "r"))
+    #parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-train_file', help="training dataset name (LPAQAfiltered41/LPAQAfiltered25 or wikidata41/wikidata25)")
+    parser.add_argument('-sample', help="set how many triple should be used of each property at maximum (e.g. 500 (=500 triples per prop for each query type) or all (= all given triples per prop for each query type))")
+    parser.add_argument('-epoch', help="set how many epoches should be executed")
+    parser.add_argument('-template', help="set which template should be used (LAMA or label)")
+    parser.add_argument('-query_type', help="set which queries should be used during training (subjobj= subject and object queries, subj= only subject queries, obj= only object queries)")
+    parser.add_argument('-transfer_learning', action="store_true", default=False, help="enables one the fly training data creation for transferlearning")
+
+    args = parser.parse_args()
+    print(args)
+    train_file = args.train_file
+    epoch = int(args.epoch)
+    template = args.template
+    sample = args.sample
+    query_type = args.query_type
+    assert(query_type in ["subjobj", "subj", "obj"])
+    transfer_learning = args.transfer_learning
+
+    #used LM
+    lm_name = 'bert-base-cased'  
+    
+    if transfer_learning:
+        #train all props if it was not already done with this setup
+        lm_name_short = lm_name.split("-")
+        lm_name_capitals = lm_name_short[0].upper()[0] + lm_name_short[1].upper()[0] + lm_name_short[2].upper()[0]
+        props_string = ""
+        model_path_all_trained = "/data/fichtel/BERTriple/models/{}F_{}_{}_{}_{}_{}{}".format(lm_name_capitals, train_file, sample, query_type, epoch, template, props_string)
+        if not os.path.exists(model_path_all_trained):
+            model_dir_all_trained = train(lm_name, train_file, sample, epoch, template, query_type, None)
+            #evaluate with LAMA
+            start_custom_model_eval(model_dir_all_trained)
+        else:
+            model_dir_all_trained = model_path_all_trained.split("/")[-1]
+        result_all_trained = dict((pd.read_csv('/home/fichtel/BERTriple/results/{}.csv'.format(model_dir_all_trained), sep = ',', header = None)).values)
+
+        protocol = {}
+        #round1: omitt props only seperately
+        round = "round1"
+        protocol[round] = []
+        all_props = ["P1001", "P106", "P1303", "P1376", "P1412", "P178", "P19", "P276", "P30", "P364", "P39", "P449", "P495", "P740", "P101", "P108", "P131", "P138", "P159", "P17", "P20", "P279", "P31", "P36", "P407", "P463", "P527", "P937", "P103", "P127", "P136", "P140", "P176", "P190", "P264", "P27", "P361", "P37", "P413", "P47", "P530"]
+        props = ["P1001", "P106", "P1303", "P1376", "P1412", "P178", "P19", "P276", "P30", "P364", "P39", "P449", "P495", "P740", "P101", "P108", "P131", "P138", "P159", "P17", "P20", "P279", "P31", "P36", "P407", "P463", "P527", "P937", "P103", "P127", "P136", "P140", "P176", "P190", "P264", "P27", "P361", "P37", "P413", "P47", "P530"]
+        for i, prop in enumerate(all_props):
+            omitted_props = [prop]
+            dictio = {}
+            dictio["omitted_props"] = omitted_props
+            #train
+            model_dir_omitted = train(lm_name, train_file, sample, epoch, template, query_type, omitted_props)
+            #evaluate with LAMA
+            start_custom_model_eval(model_dir_omitted)
+            result_omitted = dict((pd.read_csv("/home/fichtel/BERTriple/results/{}.csv".format(model_dir_omitted), sep = ',', header = None)).values)
+            dictio["tested_prop"] = {prop: {}}
+            dictio["tested_prop"][prop]["trained_prec@1"] = result_all_trained[prop]
+            dictio["tested_prop"][prop]["omitted_prec@1"] = result_omitted[prop]
+            protocol[round].append(dictio)
+        
+        #save protocol
+        protocol_file = open("/home/fichtel/BERTriple/results/transfer_learning_protocols/{}F_{}_{}_{}_{}_{}.json".format(lm_name_capitals, train_file, sample, query_type, epoch, template), "w+")
+        json.dump(protocol, protocol_file, indent=4)
+
+        #prepare round2 to remove the props for which trained_prec@1 != omitted_prec@1 and to define pairs of remaining props
+        threshold = 0.9
+        for experiment in protocol["round1"]:
+            for prop in experiment["tested_prop"]:
+                if experiment["tested_prop"][prop]["omitted_prec@1"] < threshold * experiment["tested_prop"][prop]["trained_prec@1"]:
+                    props.remove(prop)
+        print("remaining props:", props)
+        props_pairs = [(props[i],props[j]) for i in range(len(props)) for j in range(i+1, len(props))]
+
+        #round2: omitt pairs of props
+        round = "round2"
+        protocol[round] = []
+        for i, pair in enumerate(props_pairs):
+            omitted_props = list(pair)
+            dictio = {}
+            dictio["omitted_props"] = omitted_props
+            model_dir_omitted = train(lm_name, train_file, sample, epoch, template, query_type, omitted_props)
+            #evaluate with LAMA
+            start_custom_model_eval(model_dir_omitted)
+            result_omitted = dict((pd.read_csv("/home/fichtel/BERTriple/results/{}.csv".format(model_dir_omitted), sep = ',', header = None)).values)
+            dictio["tested_prop"] = {}
+            for prop in omitted_props:
+                dictio["tested_prop"][prop] = {}
+                dictio["tested_prop"][prop]["omitted_prec@1"] = result_omitted[prop]
+            protocol[round].append(dictio)
+        
+        #save protocol
+        protocol_file = open("/home/fichtel/BERTriple/results/transfer_learning_protocols/{}F_{}_{}_{}_{}_{}.json".format(lm_name_capitals, train_file, sample, query_type, epoch, template), "w+")
+        json.dump(protocol, protocol_file, indent=4)
+
+        #find groups
+        dependent_props = {}
+        threshold = 0.9
+        for prop in all_props:
+            dependent_props[prop] = []
+            for experiment in protocol["round1"]:
+                if prop in experiment["tested_prop"]:
+                    round1_omitted_prec = experiment["tested_prop"][prop]["omitted_prec@1"]
+                    break
+            print(round1_omitted_prec)
+            for experiment in protocol["round2"]:
+                if prop in experiment["tested_prop"]:
+                    round2_omitted_prec = experiment["tested_prop"][prop]["omitted_prec@1"]
+                    if round2_omitted_prec < threshold * round1_omitted_prec:
+                        omitted_props = experiment["omitted_props"].copy()
+                        omitted_props.remove(prop)
+                        dependent_props[prop].append(omitted_props[0])
+        protocol["dependent_props"] = dependent_props
+
+        #save protocol
+        protocol_file = open("/home/fichtel/BERTriple/results/transfer_learning_protocols/{}F_{}_{}_{}_{}_{}.json".format(lm_name_capitals, train_file, sample, query_type, epoch, template), "w+")
+        json.dump(protocol, protocol_file, indent=4)
+    else:
+        model_dir = train(lm_name, train_file, sample, epoch, template, query_type, None)
