4eb68f6792e190d09543e790f3f0473e3eb6009f
Leandra Fichtel <fichtel@is68.ifis.cs.tu-bs.de>
2021-06-12 00:42:50 +0200

    fixed env error

--- start_multiple_trainings.sh
+++ start_multiple_trainings.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #training and evaluation
 
-source bertriple_transformer/bin/activate
 for sample in 800 600 500 400 300 200 100 50
 do
     python add_pretraining_bert_trainer.py -train_file AUTOPROMPT41 -sample $sample -epoch 3 -template LAMA -query_type obj
