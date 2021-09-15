f7a2958811ea51ba79defb7e6c8c96c21c12b374
Leandra Fichtel <fichtel@is68.ifis.cs.tu-bs.de>
2021-09-15 14:16:46 +0200

    bug fixes

--- run_experiments.sh
+++ run_experiments.sh
@@ -20,7 +20,7 @@ python3 finetuning_and_evaluation.py -lm_name bert-base-cased -train_file AUTOPR
 python3 create_tables_for_eval.py -train_file AUTOPROMPT41 -sample all -epoch 3 -template LAMA -query_type obj -transfer_learning
 
 #experiment4 (appendix)
-python3 finetuning_and_evaluation.py -lm_name bert-large-cased -train_file AUTOPROMPT41 -sample all -epoch 3 -template label -query_type obj -vocab common -lama_uhn
-python3 finetuning_and_evaluation.py -lm_name distilbert-base-cased -train_file AUTOPROMPT41 -sample all -epoch 3 -template label -query_type obj -vocab common -lama_uhn
-python3 finetuning_and_evaluation.py -lm_name roberta-base -train_file AUTOPROMPT41 -sample all -epoch 3 -template label -query_type obj -vocab different -lama_uhn
-python3 finetuning_and_evaluation.py -lm_name facebook/bart-base -train_file AUTOPROMPT41 -sample all -epoch 3 -template label -query_type obj -vocab different -lama_uhn
+python3 finetuning_and_evaluation.py -lm_name bert-large-cased -train_file AUTOPROMPT41 -sample all -epoch 3 -template LAMA -query_type obj -vocab common -lama_uhn
+python3 finetuning_and_evaluation.py -lm_name distilbert-base-cased -train_file AUTOPROMPT41 -sample all -epoch 3 -template LAMA -query_type obj -vocab common -lama_uhn
+python3 finetuning_and_evaluation.py -lm_name roberta-base -train_file AUTOPROMPT41 -sample all -epoch 3 -template LAMA -query_type obj -vocab different -lama_uhn
+python3 finetuning_and_evaluation.py -lm_name facebook/bart-base -train_file AUTOPROMPT41 -sample all -epoch 3 -template LAMA -query_type obj -vocab different -lama_uhn
